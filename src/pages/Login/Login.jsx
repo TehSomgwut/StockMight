f3e47f252dd87c2b3371fecae54d96955cb29202
Teh Songwut <phosrisongwut@gmail.com>
2026-01-28 15:05:17 +0700

    Static Login Page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -11,33 +11,28 @@ export default function Login() {
                     <h4>ระบบจัดการคลังสินค้า</h4>
                 </div>
                 <form className="login-form">
-                    <h2>เข้าสู่ร่ะบบ</h2>
-                    <p className="description">กรุณาระบุข้อมูลเพื่อเข้าใช้งาน</p>
+                    <div>
+                        <h2>เข้าสู่ระบบ</h2>
+                        <p className="description">กรุณาระบุข้อมูลเพื่อเข้าใช้งาน</p>
+                    </div>
                     <div className="alert"><p>กรุณาระบุชื่อผู้ใช้ระรหัสผ่าน</p></div>
                     <div className="username-container">
                         <h3>ชื่อผู้ใช้</h3>
-                        <label htmlFor="username"></label>
-                        <input type="text" id="username" name="username" placeholder="กรอกชื่อผู้ใช้" />
+                        <div className="input">
+                            <label htmlFor="username"></label>
+                            <input type="text" id="username" name="username" placeholder="กรอกชื่อผู้ใช้" />
+                        </div>
                     </div>
                     <div className="password-container">
                         <h3>รหัสผ่าน</h3>
-                        <label htmlFor="password"></label>
-                        <input type="password" id="password" name="password" placeholder="กรอกรหัสผ่าน" />
-                        <img src="#" alt="eye-icon" className="eye-icon"/>
-                    </div>
-                    <button type="submit" className="login-button"><img src="#"/><h3>เข้าสู่ระบบ</h3></button>
-                    <hr/>
-                </form>
-                <div className="detail-container">
-                    <h3>ข้อมูลทดสอบ:</h3>
-                    <div className="validation-list">
-                        <div className="validation">
-                            <p className="role">ผู้ดูแลระบบ:</p>
-                            <p className="manager-name">admin / admin123</p>
+                        <div className="input">
+                            <label htmlFor="password"></label>
+                            <input type="password" id="password" name="password" placeholder="กรอกรหัสผ่าน" />
+                            <img src="#" alt="eye-icon" className="eye-icon"/>
                         </div>
                     </div>
-                    
-                </div>
+                    <button type="submit" className="login-button"><img src="#"/><h3>เข้าสู่ระบบ</h3></button>
+                </form>
             </div>
             <footer>© 2026 StockMight. ระบบจัดการคลังสินค้า</footer>
         </div>
