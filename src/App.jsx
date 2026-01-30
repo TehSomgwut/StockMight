9461546c5c61deac1abd1528b621a3b091825d87
Teh Songwut <phosrisongwut@gmail.com>
2026-01-30 12:15:16 +0700

    Test Router

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,18 @@ import './index.css';
 import Login from './pages/Login/Login';
 import Block from './components/Block/Block';
 import Side from './components/Side/Side';
+import MainPage from './pages/MainPage';
+import { Route, Routes, Link } from 'react-router-dom';
 
 export default function App() {
   return (
     <div>
-      <Login />
-      <Block />
-      <Side />
+      <Link to="/login"><h1>Login</h1></Link>
+      <Link to="/pages"><h1>Pages</h1></Link>
+      <Routes>
+        <Route path="/login" element= {<Login />} />
+        <Route path="/pages" element= {<MainPage />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,7 @@
+import { Link, Route } from 'react-router-dom';
+
+export default function MainPage() {
+    return (
+        <h1>Hello world</h1>
+    )
+}
