617fdbcc0554bc5471d90ed6141449d88715e8de
Teh Songwut <phosrisongwut@gmail.com>
2026-02-03 21:01:24 +0700

    Finally sidebar have fully animated successfully, Im so tried with this thing so long about a week. Yes!!!

--- src/components/Side/Side.jsx
+++ src/components/Side/Side.jsx
@@ -62,6 +62,7 @@ export default function Side() {
                             selectedRef={selectedRef}
                             index={index}
                             onSelect={setActiveIndex}
+                            active={activeIndex === index}
                         />
                     ))}
                 </div>
