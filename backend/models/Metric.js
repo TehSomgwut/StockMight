2d05192cabba9883d66861235557a53d0811c656
Kaxin78 <kiitinat.04@gmail.com>
2026-02-09 20:51:22 +0700

    Update Metric.js

--- backend/models/Metric.js
+++ backend/models/Metric.js
@@ -3,11 +3,8 @@ const mongoose = require("mongoose");
 const metricSchema = new mongoose.Schema({
 
   name: {type: String, required: true, unique: true},
-
   symbol: {type: String},
-
   description: {type: String},
-
   createdAt: {type: Date, default: Date.now}
 
 });
