c2d0aff3c9488272ff77a0d1a22d324dfd7b6713
Shinrakit <jackjirakit@gmail.com>
2026-03-05 13:17:18 +0700

    new Sjs

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const productRouter = require('./routes/productRouter')
 const TransactionRouter = require('./routes/transactionRouter')
 
 const app = express();
-app.set("trust proxy", 1)
+
 app.use(cors({
     origin: 'https://stockmight.vercel.app',
     credentials: true,
@@ -25,12 +25,20 @@ app.use(cors({
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "public/images")));
 
+app.set("trust proxy", 1)
+
 app.use(session({
-    secret: process.env.SESSION_SECRET || "supersecret",
-    resave: false,
-    saveUninitialized: false,
-    cookie: { secure: true, maxAge: 1000*60*60*12, httpOnly: true, }
+  secret: process.env.SESSION_SECRET || "supersecret",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: true,
+    sameSite: "none",
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 12
+  }
 }))
+
 
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRouter);
